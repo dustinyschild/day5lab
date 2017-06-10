92ee743651edee80b454e171cc925a1e5fb76b4f
Dustin <dyelland@gmail.com>
2017-06-10 01:08:24 -0500

    solved and tested problem5

--- app.js
+++ app.js
@@ -99,10 +99,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var multiplyArrayValues = [];
+  multiplyArrayValues[0] = testArray[0] + testArray[1] + testArray[2];
+  multiplyArrayValues[1] =  'The numbers ' + testArray + ' have a product of ' + multiplyArrayValues[0] + '.';
+  console.log(multiplyArrayValues);
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+multiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
